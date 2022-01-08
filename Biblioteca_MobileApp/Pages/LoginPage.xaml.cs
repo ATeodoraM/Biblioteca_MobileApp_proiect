8bc3fe1d083ae664130fc213f9ab6860fecf2390
Teodora Arseni <arseni.teodoram@gmail.com>
2022-01-08 23:56:41 +0200

    Update2

--- Biblioteca_MobileApp/Pages/LoginPage.xaml.cs
+++ Biblioteca_MobileApp/Pages/LoginPage.xaml.cs
@@ -18,7 +18,7 @@ namespace Biblioteca_MobileApp.Pages
         }
         private void Loginbtn_Clicked(object sender, EventArgs e)
         {
-            //null or empty field validation, check weather email and password is null or empty  
+            
             if (string.IsNullOrEmpty(Username.Text) || string.IsNullOrEmpty(Password.Text))
                 DisplayAlert("Username/parola invalide", "Introduceti username si parola valide", "OK");
             else
@@ -26,14 +26,14 @@ namespace Biblioteca_MobileApp.Pages
                 if (Username.Text == "administrator" && Password.Text == "1234.qwer")
                 {
                     DisplayAlert("Logat cu succes", "", "Ok");
-                    //Navigate to Wellcom page after successfully login  
-                    Navigation.PushAsync(new WelcomePageAdmin());//WelcomePage
+                   
+                    Navigation.PushAsync(new WelcomePageAdmin());
                 }
-                else if (Username.Text == "client" && Password.Text == "client")
+                else if (Username.Text == "student" && Password.Text == "123456")
                 {
                     DisplayAlert("Logat cu succes", "", "Ok");
-                    //Navigate to Wellcom page after successfully login  
-                    Navigation.PushAsync(new WelcomePageStudent());//WelcomePage2
+                  
+                    Navigation.PushAsync(new WelcomePageStudent());
                 }
                 else
                     DisplayAlert("Logare esuata", "Username/parola invalide", "OK");
